2b2dd5f25329df34eda8f26f6b5c7615b2b3c503
Tom Hsu <thomaschsu@gmail.com>
2018-06-13 00:41:03 -0400

    Modified js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 // Array of Movies
-var movies = ["The Raid 2", "Interstellar", "IT", "The Shining", "Princess Mononoke"];
+var movies = ["The Raid 2", "Interstellar", "IT", "The Shining", "Princess Mononoke", "The Dark Knight", "Pulp Fiction"];
 
 // Render the buttons function
 function renderButtons() {
     // Empty the div
     $("#movies-area").empty();
-    // Create buttons for all the movies in array
+    // Create buttons for all the movies in the movies array
     for (var i = 0; i < movies.length; i++) {
         var button = $("<button>");
         button.addClass("movie");
@@ -15,7 +15,7 @@ function renderButtons() {
     }
 }
 
-// Push movie to the array on click
+// Push movie from input to the array on click
 $("#addmovie").on("click", function(event) {
     event.preventDefault();
     var movie = $("#movie-input").val().trim();
@@ -26,38 +26,27 @@ $("#addmovie").on("click", function(event) {
 // Starts renderbuttons function
 renderButtons()
 
+// When clicking any button, use the api to create divs and append it to the page
 $("button").on("click", function() {
-
+    // Empty div first
+    $("#gifs").empty();
     var moviename = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + moviename + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        moviename + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    // Performing an AJAX request with the queryURL
+    // Performing ajax request
     $.ajax({
             url: queryURL,
             method: "GET"
         })
-        // After data comes back from the request
         .then(function(response) {
-
             var results = response.data;
-
             for (var i = 0; i < results.length; i++) {
-
                 var movieDiv = $("<div>");
-
                 var p = $("<p>").text("Rating: " + results[i].rating);
-
-                // Creating and storing an image tag
                 var movieImage = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
                 movieImage.attr("src", results[i].images.fixed_height.url);
-
-                // Appending the paragraph and image tag to the animalDiv
                 movieDiv.append(p);
                 movieDiv.append(movieImage);
-
                 $("#gifs").prepend(movieDiv);
             }
         });
